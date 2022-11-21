035df948298e78a0f20a549f87fa6d7bfaaeb8e1
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2022-11-21 14:43:09 +0530

    Add files via upload

--- derm_scrape/derm_scrape.py
+++ derm_scrape/derm_scrape.py
@@ -2,35 +2,42 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import urllib.request
+import csv
 
 chromedriver= "chromedriver.exe"
 browser = webdriver.Chrome(chromedriver)
 browser.get("https://dermnetnz.org/image-library")
 
-# imgResults = browser.find_elements(By.XPATH,"//img")
+imgResults = browser.find_elements(By.TAG_NAME,"img")
 
-# # Access and store the scr list of image url's.
-# src = []
-# for img in imgResults[:293]:
-#     src.append(img.get_attribute('src'))
+# Access and store the scr list of image url's.
+src = []
+for img in imgResults[2:294]:
+    src.append(img.get_attribute('src'))
 
-# opener = urllib.request.build_opener()
-# opener.addheaders = [('User-Agent', 'MyApp/1.0')]
-# urllib.request.install_opener(opener)
+opener = urllib.request.build_opener()
+opener.addheaders = [('User-Agent', 'MyApp/1.0')]
+urllib.request.install_opener(opener)
 
-# # Retrieve and download the images.
-# for i in range(len(src)):    
-#     urllib.request.urlretrieve(str(src[i]),"sample_data/derm{}.jpg".format(i+1))
+# downloading the images.
+for i in range(len(src)):    
+    urllib.request.urlretrieve(str(src[i]),"sample_data/derm{}.png".format(i+1))
 
-# list of name of disease, url links, icon
+# list of name of disease
 dis = []
 names = browser.find_elements(By.XPATH,"//h6")
 for name in names:
     dis.append(name.text)
 # print(len(dis))
 
-# dst = {}
-# for i,j in zip(dis, src):
-#     dst[i] = j
+# mapping disease names to url image links
+dst = {}
+for i,j in zip(dis, src):
+    dst[i] = j
+# print(dst) 
 
-# print(dst)    
+# storing dictionary to csv
+with open('test.csv', 'w') as csv_file:  
+    writer = csv.writer(csv_file)
+    for key, value in dst.items():
+       writer.writerow([key, value])   
